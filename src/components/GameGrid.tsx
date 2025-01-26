c008306b236de6573984c2e9e3597b58ec2c9579
Behnam <behnam.sep@gmail.com>
2025-01-26 19:13:05 +0100

    Displaying platform icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -8,10 +8,23 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10} padding={10}>
-        {games.map((game: { id: number; name: string; background_image: string }) => (
-          <GameCard key={game.id} game={game}/>
-        ))}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={10}
+        padding={10}
+      >
+        {games.map(
+          (game: {
+            id: number;
+            name: string;
+            background_image: string;
+            parent_platforms: {
+              platform: { id: number; name: string; slug: string };
+            }[];
+          }) => (
+            <GameCard key={game.id} game={game} />
+          )
+        )}
       </SimpleGrid>
     </>
   );
