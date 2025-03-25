85bae2f8e9d274daec9c5cc56d3910a52eebd4d6
Behnam <behnam.sep@gmail.com>
2025-03-25 14:57:57 +0100

    Add SearchInput component and update tsconfig for new components

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,15 +1,14 @@
-import useGames, { Platform } from "../hooks/useGames";
+import useGames from "../hooks/useGames";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenres";
 import { GameQuery } from "../App";
 
 interface Props {
   gameQuery: GameQuery;
 }
-const GameGrid = ({gameQuery }: Props) => {
+const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
