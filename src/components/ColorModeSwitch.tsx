b9188e5e4060022e491348efdd3ca6c17c9dc135
Behnam <behnam.sep@gmail.com>
2025-03-25 15:08:57 +0100

    Add SearchInput component to Navbar and improve ColorModeSwitch layout

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -9,7 +9,7 @@ const ColorModeSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text>{colorMode === "dark" ? "Dark Mode" : "Light Mode"}</Text>
+      <Text whiteSpace='nowrap'>{colorMode === "dark" ? "Dark Mode" : "Light Mode"} </Text>
     </HStack>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Img } from "@chakra-ui/react";
 import logo from "./assets/Logo/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 const Navbar = () => {
   return (
-    <HStack justifyContent="space-between" padding="12px">
+    <HStack padding="12px">
       <Img src={logo} alt="logo" boxSize="60px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
