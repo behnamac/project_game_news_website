77bc01096f1035e136e9a82bf315bcb5126a2c6a
Behnam <behnam.sep@gmail.com>
2025-06-19 15:15:51 +0200

    Add @tanstack/react-query for data fetching and wrap App in QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.tsx";
 import theme from "./theme.ts";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <QueryClientProvider client={queryClient}>
       <App />
-    </ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
